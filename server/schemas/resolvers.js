2fd81b65057ea0474a4ad978e667eff8be2f6dba
lorreina <slorreina369@gmail.com>
2022-09-25 12:18:15 -0700

    added resolvers page to work with typedefs confirmed works in apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,47 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+    Query:{
+        // me: async({user, params}, res) =>{
+        //     if(context.user){
+        //         const userData = await User.findOne({_id:context.user._id})
+        //             .select('-_v -password')
+        //     }
+        // }
+        users:async() =>{
+            return User.find()
+                .select('-_v -password')
+                .populate('book');
+        },
+        user:async(parent, { username }) =>{
+            return User.findOne({ username })
+                .select('-_v -password')
+                .populate('books');
+        }
+    },
+    Mutations:{
+        addUser:async(parent, args) =>{
+            const user = await User.create(args);
+            
+            return { user };
+        },
+        login: async(parents, { email, password }) =>{
+            const user = await User.findOne({email});
+            if(!user){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return { user };
+        }
+
+    }
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,16 +9,31 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+const startApollowServer = async(typeDefs, resolvers) =>{
+  await server.start();
+
+  server.applyMiddleware({app});
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApollowServer(typeDefs, resolvers)
