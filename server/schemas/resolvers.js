03f7c4b29620255b09393f9a89ab62e3e551cbe2
lorreina <slorreina369@gmail.com>
2022-09-25 13:41:40 -0700

    added token for loggins and addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query:{
@@ -23,8 +24,9 @@ const resolvers = {
     Mutation:{
         addUser:async(parent, args) =>{
             const user = await User.create(args);
+            const token = signToken(user);
             
-            return user;
+            return { token, user };
         },
         login: async(parents, { email, password }) =>{
             const user = await User.findOne({email});
@@ -38,7 +40,9 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
-            return user;
+            const token = signToken(user);
+
+            return { token, user };
         }
 
     }
